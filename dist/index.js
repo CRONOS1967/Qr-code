94a7d05b4d4a0d1e030f7a7eebc526bccb98e32c
crono1967 <kabila.desalgn@gmail.com>
2021-12-16 02:53:57 +0300

    fixed issue on showing and hiding success message

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,33 @@
 "use strict";
-let btn = document.getElementById('gen');
-let qrcode = document.getElementById('qr');
-let input = document.getElementById('input');
-btn === null || btn === void 0 ? void 0 : btn.addEventListener('click', generate);
+let btn = document.getElementById("gen");
+let qrcode = document.getElementById("qr");
+let input = document.getElementById("input");
+let done = document.getElementById("done");
+let undone = document.getElementById("undone");
+btn === null || btn === void 0 ? void 0 : btn.addEventListener("click", generate);
 function generate() {
     let data = input.value;
     console.log(data);
     var url = `https://api.qrserver.com/v1/create-qr-code/?data=${data}`;
     qrcode.src = url;
+    console.log(qrcode.src);
+    if (qrcode.src == null) {
+        unpop();
+    }
+    else {
+        popup();
+    }
+}
+function popup() {
+    setTimeout(() => {
+        done === null || done === void 0 ? void 0 : done.classList.remove("visually-hidden");
+    }, 2000);
+    setTimeout(() => {
+        done === null || done === void 0 ? void 0 : done.classList.add("visually-hidden");
+    }, 4900);
+}
+function unpop() {
+    setTimeout(() => {
+        undone === null || undone === void 0 ? void 0 : undone.classList.remove("visually-hidden");
+    }, 5000);
 }
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,35 @@
-let btn = document.getElementById('gen');
-let qrcode:any = document.getElementById('qr');
-let input:any = document.getElementById('input')
+let btn = document.getElementById("gen");
+let qrcode: any = document.getElementById("qr");
+let input: any = document.getElementById("input");
+let done = document.getElementById("done");
+let undone = document.getElementById("undone");
+btn?.addEventListener("click", generate);
 
-btn?.addEventListener('click',generate)
+function generate() {
+  let data = input.value;
+  console.log(data);
+  var url = `https://api.qrserver.com/v1/create-qr-code/?data=${data}`;
+  qrcode.src = url;
+  console.log(qrcode.src);
+  
+  if (qrcode.src == null) {
+      unpop();
+} else {
+      popup();
+  }
+}
 
-function generate(){
-    let  data = input.value;
-    console.log(data)
-    var url = `https://api.qrserver.com/v1/create-qr-code/?data=${data}`
-    qrcode.src= url;
+function popup() {
+  setTimeout(() => {
+    done?.classList.remove("visually-hidden");
+  }, 2000);
+
+  setTimeout(() => {
+    done?.classList.add("visually-hidden");
+  }, 4900);
+}
+function unpop() {
+  setTimeout(() => {
+    undone?.classList.remove("visually-hidden");
+  }, 5000);
 }
